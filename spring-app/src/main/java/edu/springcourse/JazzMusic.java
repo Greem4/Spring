b443dafbc67ef21a5cf68addbd4de51bd21b4c69
Greem4 <i@greem4.ru>
2024-05-16 19:48:18 +0300

    :building_construction: spring r6 dz

--- spring-app/src/main/java/edu/springcourse/JazzMusic.java
+++ spring-app/src/main/java/edu/springcourse/JazzMusic.java
@@ -4,6 +4,6 @@ public class JazzMusic implements Music {
 
     @Override
     public String getSong() {
-        return "song jazz music";
+        return "Hit the Road Jack";
     }
 }
--- spring-app/src/main/java/edu/springcourse/MusicPlayer.java
+++ spring-app/src/main/java/edu/springcourse/MusicPlayer.java
@@ -1,7 +1,10 @@
 package edu.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
@@ -22,19 +25,20 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public MusicPlayer() {
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println(music.getSong());
+        }
     }
 }
