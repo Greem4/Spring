5bb38d288357ce9bffe6bbf55d582d44d92251b1
Greem4 <i@greem4.ru>
2024-05-18 15:00:20 +0300

    :construction: spring r10

--- spring-app/src/main/java/edu/springcourse/ClassicalMusic.java
+++ spring-app/src/main/java/edu/springcourse/ClassicalMusic.java
@@ -1,5 +1,8 @@
 package edu.springcourse;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
