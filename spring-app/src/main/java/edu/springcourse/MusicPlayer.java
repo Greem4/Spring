8f0c6c3a8cb18e28d120b2350674faf061de0361
Greem4 <i@greem4.ru>
2024-05-19 22:50:48 +0300

    :construction: spring r14 dz

--- spring-app/src/main/java/edu/springcourse/MusicPlayer.java
+++ spring-app/src/main/java/edu/springcourse/MusicPlayer.java
@@ -4,40 +4,20 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MusicPlayer {
-    @Value("${musicPayer.name}")
-    private String name;
+    private List<Music> musicList;
 
-    @Value("${musicPayer.volume}")
-    private int volume;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-}
-
-    private Music music1;
-    private Music music2;
-
-    public MusicPlayer(@Qualifier("rockMusic") Music music1,
-                       @Qualifier("classicalMusic") Music music2) {
-        this.music1 = music1;
-        this.music2 = music2;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public String playMusic() {
-        return "Playing: " + music1.getSong() + ", " + music2.getSong();
-    }
+        Random random = new Random();
 
+        return "Playing: " + musicList.get(random.nextInt(musicList.size())).getSong();
+    }
 }
--- spring-app/src/main/java/edu/springcourse/SpringConfig.java
+++ spring-app/src/main/java/edu/springcourse/SpringConfig.java
@@ -1,9 +1,11 @@
 package edu.springcourse;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @PropertySource("classpath:musicPlayer.properties")
@@ -17,10 +19,19 @@ public class SpringConfig {
     public RockMusic rockMusic() {
         return new RockMusic();
     }
+    @Bean
+    public JazzMusic jazzMusic() {
+        return new JazzMusic();
+    }
+
+    @Bean
+    public List<Music> musicList() {
+        return Arrays.asList(classicalMusic(), rockMusic(), jazzMusic());
+    }
 
     @Bean
     public MusicPlayer musicPlayer() {
-        return new MusicPlayer(rockMusic(), classicalMusic());
+        return new MusicPlayer(musicList());
     }
 
     @Bean
