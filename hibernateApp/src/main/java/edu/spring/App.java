238d199a2aa217b0f9e54a58a8699f911858ccea
Greem4 <i@greem4.ru>
2024-06-02 11:39:02 +0300

    :recycle:  r 56 spring manyToMany

--- hibernateApp/src/main/java/edu/spring/App.java
+++ hibernateApp/src/main/java/edu/spring/App.java
@@ -1,10 +1,16 @@
 package edu.spring;
 
+import edu.spring.model.Actor;
+import edu.spring.model.Movie;
 import edu.spring.model.Passport;
 import edu.spring.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -12,28 +18,46 @@ import org.hibernate.cfg.Configuration;
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Movie.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
 
-        try {
+        try (sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-//            Person person = new Person("Test cascading", 30);
-//            Passport passport = new Passport(123456);
+//            Movie movie = new Movie("Pulp fiction", 1994);
+//            Actor actor1 = new Actor("Harvey Keitel", 81);
+//            Actor actor2 = new Actor("Samuel L. Jackson", 72);
 //
-//            person.setPassport(passport);
-//            session.save(person);
+//            movie.setActors(new ArrayList<>(List.of(actor1,actor2)));
+//
+//            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//
+//            session.save(movie);
+//            session.save(actor1);
+//            session.save(actor2);
 
-            Person person = session.get(Person.class, 1);
-            session.remove(person);
+//            Movie movie = new Movie("Reservoir Dogs", 1992);
+//            Actor actor = session.get(Actor.class, 1);
+//
+//            movie.setActors(new ArrayList<>(Collections.singletonList(actor)));
+//
+//            actor.getMovies().add(movie);
+//
+//            session.save(movie);
+
+            Actor actor = session.get(Actor.class, 2);
+            System.out.println(actor.getMovies());
+
+            Movie movieToRemove = actor.getMovies().get(0);
+
+            actor.getMovies().remove(0);
+            movieToRemove.getActors().remove(actor);
 
             session.getTransaction().commit();
-
-        } finally {
-            session.close();
         }
     }
 }
