6ffd5ca75bb311dc40d30c644bc71399ddf2a2b6
Greem4 <i@greem4.ru>
2024-06-13 18:47:17 +0300

    :recycle: r 80 spring security delete AuthProviderImpl and refactor SecurityConfig

--- FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/config/SecurityConfig.java
@@ -1,20 +1,32 @@
 package edu.spring.firstsecurityapp.config;
 
-import edu.spring.firstsecurityapp.security.AuthProviderImpl;
+import edu.spring.firstsecurityapp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-        http.authenticationProvider(authProvider);
+        http.authenticationProvider((AuthenticationProvider) personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
