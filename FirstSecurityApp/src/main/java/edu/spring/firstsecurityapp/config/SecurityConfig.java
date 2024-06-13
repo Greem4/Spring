a9ea9cc0b6b06138500bb607a2d1d740b04a24f9
Greem4 <i@greem4.ru>
2024-06-13 21:27:50 +0300

    :recycle: r 81 spring security

--- FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/config/SecurityConfig.java
@@ -5,14 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig   {
 
     private final PersonDetailsService personDetailsService;
 
@@ -21,9 +24,24 @@ public class SecurityConfig {
         this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authenticationProvider((AuthenticationProvider) personDetailsService);
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // конфигурируем сам Spring Security
+        // конфигурируем авторизацию
+        http.csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+        return http.build();
     }
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
