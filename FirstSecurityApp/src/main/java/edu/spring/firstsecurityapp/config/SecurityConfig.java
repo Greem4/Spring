15ea3eb3974e6324b36fad687731e16ad0d3b4ae
Greem4 <i@greem4.ru>
2024-06-14 16:02:13 +0300

    :construction: r 87 spring security add ROLE_ADMIN ver 2

--- FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import edu.spring.firstsecurityapp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final PersonDetailsService personDetailsService;
@@ -28,7 +30,6 @@ public class SecurityConfig {
         // конфигурируем сам Spring Security
         // конфигурируем авторизацию
         http.authorizeRequests(authorize -> authorize
-                        .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                         .anyRequest().authenticated()
                 )
--- FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/controllers/HelloController.java
+++ FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/controllers/HelloController.java
@@ -1,6 +1,8 @@
 package edu.spring.firstsecurityapp.controllers;
 
 import edu.spring.firstsecurityapp.security.PersonDetails;
+import edu.spring.firstsecurityapp.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -25,6 +33,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
