ba8f042acf89bc476ba2e1c76b2a89229c3dff8a
Greem4 <i@greem4.ru>
2024-06-14 11:08:10 +0300

    :construction: r 83 spring security add "logout"

--- FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/edu/spring/firstsecurityapp/config/SecurityConfig.java
@@ -37,7 +37,10 @@ public class SecurityConfig   {
                 .formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/hello", true)
-                .failureUrl("/auth/login?error");
+                .failureUrl("/auth/login?error")
+                .and()
+                .logout().logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login");
         return http.build();
     }
 
